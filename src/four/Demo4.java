c72d5d7c0aff44cb4057fe284d7fd3d8c9fa7641
agg72 <agg72@DESKTOP-BN6NDO2>
2021-11-24 20:02:35 -0500

    practicing

--- src/four/Demo4.java
+++ src/four/Demo4.java
@@ -5,6 +5,10 @@ public class Demo4 {
 	public static void main(String[] args) {
 		System.out.println("Push 4 ");
 		System.out.println("adding");
+		
+		System.out.println("More practice");
 	}
+	
+	
 
 }
